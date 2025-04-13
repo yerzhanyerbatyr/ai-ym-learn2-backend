9872fb05090ab3fad7169ee7f522413adc69e08d
sdilnaz <dilnaz.sembekova@gmail.com>
2025-04-14 01:09:04 +0500

    update course model

--- src/models/courseModel.ts
+++ src/models/courseModel.ts
@@ -39,6 +39,7 @@ export interface ICourse extends Document {
   createdBy: string;
   createdAt: Date;
   updatedAt: Date;
+  isPublished: boolean;
 }
 
 const exerciseSchema = new Schema<IExercise>({
@@ -80,6 +81,7 @@ const courseSchema = new Schema<ICourse>({
   createdBy: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  isPublished: { type: Boolean, default: false }
 },
 {
   timestamps: true
